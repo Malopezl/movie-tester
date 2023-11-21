a6d9b167b5b35122289482173d386d99632df4df
Marvin Lopez <lopez.marvin9@gmail.com>
2023-11-21 00:19:28 -0600

    understanding assertequals from junit

--- src/test/java/gt/com/archteam/util/StringUtilTest.java
+++ src/test/java/gt/com/archteam/util/StringUtilTest.java
@@ -3,14 +3,13 @@ package gt.com.archteam.util;
 public class StringUtilTest {
 
     public static void main(String[] args) {
-        String result = StringUtil.repeat("hola", 3);
-        if (!result.equals("holaholahola")) {
-            System.out.println("ERROR");
-        }
+        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+        assertEquals(StringUtil.repeat("hola", 1), "hola");
+    }
 
-        String result2 = StringUtil.repeat("hola", 1);
-        if (!result2.equals("hola")) {
-            System.out.println("ERROR");
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)) {
+            throw new RuntimeException(actual + " is not equal to " + expected);
         }
     }
 
