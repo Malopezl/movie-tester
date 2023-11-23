11bfa224cf5b69fca86354750530c3909cb1d4cd
Marvin Lopez <lopez.marvin9@gmail.com>
2023-11-23 11:24:20 -0600

    use mockito to simulate a dice

---
+++ src/main/java/gt/com/archteam/mockito/Dice.java
@@ -0,0 +1,16 @@
+package gt.com.archteam.mockito;
+
+import java.util.Random;
+
+public class Dice {
+    private int sides;
+    private Random random = new Random();
+
+    public Dice(int sides) {
+        this.sides = sides;
+    }
+
+    public int roll() {
+        return this.random.nextInt(sides) + 1;
+    }
+}
---
+++ src/test/java/gt/com/archteam/mockito/PlayerTest.java
@@ -0,0 +1,28 @@
+package gt.com.archteam.mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class PlayerTest {
+    @Test
+    public void loses_when_dice_number_is_too_low() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+
+        Player player = new Player(dice, 3);
+        assertFalse(player.play());
+    }
+
+    @Test
+    public void wins_when_dice_number_is_big() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+    }
+}
