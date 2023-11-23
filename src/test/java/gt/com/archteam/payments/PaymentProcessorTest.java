b553efaee558dfb9e5b32597f3f7e16fe77f9354
Marvin Lopez <lopez.marvin9@gmail.com>
2023-11-23 13:18:41 -0600

    improve payment tests and string validations

--- src/test/java/gt/com/archteam/payments/PaymentProcessorTest.java
+++ src/test/java/gt/com/archteam/payments/PaymentProcessorTest.java
@@ -3,26 +3,32 @@ package gt.com.archteam.payments;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_successful() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertTrue(paymentProcessor.makePayment(1000));
     }
 
     @Test
     public void payment_unsuccessful() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
--- src/test/java/gt/com/archteam/util/StringUtilTest.java
+++ src/test/java/gt/com/archteam/util/StringUtilTest.java
@@ -1,5 +1,8 @@
 package gt.com.archteam.util;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,6 +26,26 @@ public class StringUtilTest {
     @Test(expected = IllegalArgumentException.class)
     public void repeat_string_negative_times() {
         StringUtil.repeat("hola", -1);
+    }
+
+    @Test
+    public void string_is_not_empty() {
+        assertFalse(StringUtil.isEmpty("hola mundo"));
+    }
+
+    @Test
+    public void string_is_empty() {
+        assertTrue(StringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void null_string_is_empty() {
+        assertTrue(StringUtil.isEmpty(null));
+    }
+
+    @Test
+    public void string_with_spaces_is_empty() {
+        assertTrue(StringUtil.isEmpty(" "));
     }
 
 }
