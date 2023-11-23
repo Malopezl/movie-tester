05709cea84cfc546f71c49d695c3d1253fda38af
Marvin Lopez <lopez.marvin9@gmail.com>
2023-11-23 10:53:26 -0600

    test to verify passwords

---
+++ src/main/java/gt/com/archteam/util/PasswordUtil.java
@@ -0,0 +1,22 @@
+package gt.com.archteam.util;
+
+public class PasswordUtil {
+
+    public enum SecurityLevel {
+        WEAK,
+        MEDIUM,
+        STRONG
+    }
+
+    public static SecurityLevel assessPassword(String password) {
+        if (password.length() < 8 || password.matches("[a-zA-Z]+")) {
+            return SecurityLevel.WEAK;
+        }
+
+        if (password.length() < 8 || password.matches("[a-zA-Z0-9]+")) {
+            return SecurityLevel.MEDIUM;
+        }
+        return SecurityLevel.STRONG;
+    }
+
+}
